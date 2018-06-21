9d4b98f06187d4635a7e5d5a5114a230e4ea94f4
Glitch (shine-piranha) <none>
2018-06-21 14:24:42 +0000

    🍦🍹 Checkpoint ./server.js:1284768/318

--- server.js
+++ server.js
@@ -26,17 +26,18 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/timestamp/:date_string", function (req, res) {
-  var tipo =typeof req.params.date_string;
-  console.log(tipo);
-  var tipo_depois = Number(req.params.date_string);
-  console.log(tipo_depois);
-  var date_string = new Date(req.params.date_string);
-   var c=Number(date_string);
+  
+  var date_string = Number(req.params.date_string);
+  console.log(date_string);
+  if(date_string =='
+  
+  //var = new Date(date_string);
+  // var c=Number(date_string);
   
 
   
   res.json({
-    unix: c ,
+    unix: date_string ,
     utc: date_string.toUTCString()
   });
 });
